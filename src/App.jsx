e22f27bbf4adc34323a92685ee160d957a3f5da2
nanda-98 <ajaynandakumard@gmail.com>
2024-12-12 14:20:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import  GameBoard  from "./components/GameBoard";
 import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
-  function handleSelectSquare(){
+
+  function handleSelectSquare(rowIndex, colIndex){
      setActivePlayer((curActivePlayer)=>curActivePlayer==='X'?'O':'X');
-     setGameTurns();
+     setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length>0 && prevTurns[0].Player === 'X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        {square:{row: rowIndex, col: colIndex}, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+     });
   }
   return (
     <main>
